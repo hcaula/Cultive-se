7a0e8e647a3d5121aa6d02d4f759ab8879212b94
Henrique Caúla <lhtc@cin.ufpe.br>
2019-11-09 18:06:48 -0300

    Open connection to WebSocket

---
+++ pushover.js
@@ -0,0 +1,36 @@
+const https = require('https');
+
+const HOSTNAME = 'api.pushover.net';
+const RESOURCE = '/1/messages.json';
+const TOKEN = 'as54gyap1wpj6f1p5tsp5h1xpazxay';
+const USER = 'ut2o97cbhfokgi6st4ah8obfwqhw3e';
+
+const query_string = `?token=${TOKEN}&user=${USER}&`;
+const path = `${RESOURCE}${query_string}`;
+const options = {
+  hostname: HOSTNAME,
+  method: 'POST'
+};
+
+const sendNotification = message => {
+  const messageQueryString = `message=${message}`;
+  const full_path = `${path}${messageQueryString}`;
+  options.path = full_path;
+
+  const req = https.request(options, res => {
+    res.on('data', d => {
+      process.stdout.write(d);
+    });
+  });
+
+  req.on('error', error => {
+    console.error(error);
+  });
+
+  req.write(message);
+  req.end();
+};
+
+module.exports = {
+  sendNotification
+};
