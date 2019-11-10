71cfe90d083587d9c58ccee6cbb768d3ee8e50fe
Henrique Caúla <lhtc@cin.ufpe.br>
2019-11-10 15:10:32 -0300

    Calls initWebSocket function

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const initMqtt = require('./modules/mqtt')
+const { initWebSocket } = require('./modules/websocket')
 const http = require('http')
 
 // Init server for heroku
 const server = http.createServer().listen(process.env.PORT || 8000)
+initWebSocket(server)
 
 initMqtt(server)
