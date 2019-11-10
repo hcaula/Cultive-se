56d497d65b64633900f06ee285c3342d24916519
Henrique Caúla <lhtc@cin.ufpe.br>
2019-11-10 10:31:37 -0300

    Use MQTT to receive data from Rasp

--- server/index.js
+++ server/index.js
@@ -1,20 +1,3 @@
-const { getBadMetrics } = require('./analyze')
+const initMqtt = require('./mqtt')
 
-/* Connect to Cutive-se Raspberry Pi Websocket server */
-const WebSocket = require('ws')
-const ws = new WebSocket('ws://10.0.202.253:1880/ws/simple')
-
-ws.onopen = () => {
-  console.log('Connection openned :D')
-}
-
-ws.onclose = () => {
-  console.error('Connection closed :c')
-}
-
-ws.onmessage = ({ data }) => {
-  data = JSON.parse(data)
-  const badMetrics = getBadMetrics(data)
-
-  console.log(badMetrics)
-}
+initMqtt()
---
+++ server/mqtt.js
@@ -0,0 +1,35 @@
+const mqtt = require('mqtt')
+const { getBadMetrics } = require('./analyze')
+
+const IS_LOCAL = true
+const PROTOCOL = 'mqtt://'
+const HIVE = 'broker.hivemq.com'
+const LOCAL_TOPICS = ['hackadeira/local']
+const RASP_TOPICS = ['hackadeira/estufa']
+
+const uri = `${PROTOCOL}${HIVE}`
+
+const init = () => {
+  const client = mqtt.connect(uri)
+
+  client.on('connect', () => {
+    console.log(`Connected to ${HIVE} :D`)
+
+    const topics = IS_LOCAL ? LOCAL_TOPICS : RASP_TOPICS
+    client.subscribe(topics, err => {
+      if (err) throw err
+      else {
+        topics.forEach(topic =>
+          console.log(`Successfully subscribed to ${topic} :D`)
+        )
+      }
+    })
+  })
+
+  client.on('message', (topic, message) => {
+    console.log(`Received message from topic ${topic}`)
+    const data = JSON.parse(message)
+  })
+}
+
+module.exports = init
